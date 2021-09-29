d6a02c2275cf83e8adfd9a684e3e69e9d6234899
Tiffany_Tam <tamtiffany89@gmail.com>
2021-09-29 00:37:48 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+const employee = {
+    name: "Tiffany",
+    streetAddress: "160 W 96TH ST",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = { ...employee };
+
+    newObj[key] = value;
+  
+    return newObj;
+  }
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+};
+
+function deleteFromEmployeeByKey (employee, key, value) {
+    const newObj = {employee};
+
+    newObj[key] = value;
+
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    const newObj = {employee};
+    employee[key] = value;
+
+    return employee;
+}
